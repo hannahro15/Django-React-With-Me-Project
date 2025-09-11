3a824bfebdfee7aee8018eb4bdfa699cd3464b1a
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-11 19:22:17 +0100

    Add some styling and a background-image to the home page

--- frontend/reactapp/src/pages/home/Home.jsx
+++ frontend/reactapp/src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import Button from 'react-bootstrap/Button';
+import './Home.css';
 
 function Home() {
     return (
-        <div className='Home'>
-            <h1>Welcome to Milton Keynes</h1>
-            <p>Please view the attractions below by clicking the button to take you to the attractions page.</p>
-            <Button variant="secondary">View Attractions</Button>
-        </div>
+        <div className='home'>
+            <div className='home-container'>
+                <h1 id="home-title"> Welcome to Milton Keynes</h1>
+                <p id="home-paragraph">Please view the attractions below by clicking the button to take you to the attractions page.</p>
+                <Button variant="secondary" id="home-page-button">View Attractions</Button>
+            </div>
+        </div >
     )
 }
 
