ade9e35ae7227bc01e7c10d0be0565990be63160
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-11 14:16:25 +0100

    Install react, and connect to Django

--- backend/with_me_project/with_me_project/urls.py
+++ backend/with_me_project/with_me_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', ('home.urls'))
+    path('home/', ('home.urls')),
+    path('', TemplateView.os_view(template_name='index.html'))
 ]
