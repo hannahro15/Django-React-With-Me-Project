d5a81262344f143d28b45b5dabdcad079ccd2914
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-12 12:11:50 +0100

    Create sign in form

--- frontend/reactapp/src/pages/register-signin/Sign-in.jsx
+++ frontend/reactapp/src/pages/register-signin/Sign-in.jsx
@@ -1,5 +1,24 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Sign-in.css'
 
 function Signin() {
+  return (
+    <Form className="signin">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label className="form-label">Email address: </Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
 
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label className="form-label">Password: </Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Sign In
+      </Button>
+    </Form> 
+  );
 }
+
 export default Signin;
