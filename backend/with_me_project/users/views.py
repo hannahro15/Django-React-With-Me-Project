8c31dffbbe8b1c1565bf85301a0f1d47a6d5b820
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-12 13:07:54 +0100

    Add view for registration form

--- backend/with_me_project/users/views.py
+++ backend/with_me_project/users/views.py
@@ -4,8 +4,20 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from .models import Register
 
+# Create your views here.
 
-# Create your views here.r
+def registration(request):
+    if request.method=="POST":
+        post=Register()
+        post.user_name=request.POST['username']
+        post.email=request.POST['email']
+        post.dateofbirth=request.POST['date of birth']
+        post.country=request.POST['country']
+        post.save()
+        return render(request, '/Register.jsx')
+    else:
+        return render(request, '/Register.jsx')
 
 
