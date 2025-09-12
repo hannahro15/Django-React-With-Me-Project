a4881ab61466ebc72e081cfc50b98c1009fa3b23
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-12 10:36:48 +0100

    Add to requirements.txt

--- backend/with_me_project/with_me_project/urls.py
+++ backend/with_me_project/with_me_project/urls.py
@@ -17,11 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', ('home.urls')),
-    path('', TemplateView.os_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('accounts/', include('allauth.urls'))
 ]
