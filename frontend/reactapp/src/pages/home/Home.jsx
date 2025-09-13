8f27bdf52ff3a85e7e28453a28c3748de7008f3e
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-13 18:11:26 +0100

    Get link to attractions page working from home page

--- frontend/reactapp/src/pages/home/Home.jsx
+++ frontend/reactapp/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
@@ -7,7 +7,7 @@ function Home() {
             <div className='home-container'>
                 <h1 id="home-title"> Welcome to Milton Keynes</h1>
                 <p id="home-paragraph">Please view the attractions below by clicking the button to take you to the attractions page.</p>
-                <Button variant="secondary" id="home-page-button">View Attractions</Button>
+                <Link to="/attractions" className="btn btn-secondary">View Attractions</Link>
             </div>
         </div >
     )
