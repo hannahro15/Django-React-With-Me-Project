3d0d717d91cd434c67ad496fc0868cfc7745e86a
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-13 14:44:43 +0100

    Fix typo in urls.py

--- backend/with_me_project/with_me_project/urls.py
+++ backend/with_me_project/with_me_project/urls.py
@@ -16,12 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', ('home.urls')),
     path('accounts/', include('allauth.urls')),
-    path('users/', include('users.urls')),
+    path('api/users/', include('users.urls')),
 ]
