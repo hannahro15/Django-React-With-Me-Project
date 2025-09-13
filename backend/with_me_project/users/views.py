6e7da16306a01e91f774900be4a45834f920815e
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-13 14:00:55 +0100

    Add views and admin.py for register and login

--- backend/with_me_project/users/views.py
+++ backend/with_me_project/users/views.py
@@ -1,23 +1,31 @@
-from django.shortcuts import render, request
-from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token
+from rest_framework import status
 from django.contrib.auth.models import User
-from .models import Register
+from .serializers import UserSerializer, LoginSerializer
+
 
 # Create your views here.
 
-def registration(request):
-    if request.method=="POST":
-        post=Register()
-        post.user_name=request.POST['username']
-        post.email=request.POST['email']
-        post.dateofbirth=request.POST['date of birth']
-        post.country=request.POST['country']
-        post.save()
-        return render(request, '/Register.jsx')
-    else:
-        return render(request, '/Register.jsx')
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data 
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
